6b45bfc2d32196fcfcee60ec59c95468a347a5c8
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2023-10-01 14:47:45 +0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -67,7 +67,7 @@ export default function Intro() {
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Angular (NG-ZORRO)</span>.
       </motion.h1>
 
       <motion.div
